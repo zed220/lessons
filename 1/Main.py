ffe8e4274ac730d31fb11f4d50f1c8502015c7d8
pzed220 <pzed220@gmail.com>
2019-05-16 21:13:34 +0300

    add project

--- 1/Main.py
+++ 1/Main.py
@@ -1,5 +1,10 @@
-print("Первая программа Артёма")
-print("Считаем сумму")
-first = input("Введите первое число:")
-second = input("Введите второе число:")
-print(f"Сумма: {int(first) + int(second)}")
+
+def Main():
+    print("Первая программа Артёма")
+    print("Считаем сумму")
+    first = input("Введите первое число:")
+    second = input("Введите второе число:")
+    print(f"Сумма: {int(first) + int(second)}")
+    pass
+
+Main()
--- 1/MainTest.py
+++ 1/MainTest.py
@@ -1,6 +1,18 @@
 import unittest
+from unittest.mock import patch
+import Main
 
 class Testing(unittest.TestCase):
-    
+
+
     def test_input_2_values(self):
+        user_input = [ 55, 44 ]
+        with patch('builtins.input', side_effect=user_input):
+            Main.Main()
+        self.assertEqual(stacks, expected_stacks)
+        
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
